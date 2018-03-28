ed1d70c8b0c199947ca6f64301f0987681bc1978
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-03-28 22:36:03 +0300

    Add bundle ID validation

--- Task 1/Assets/Editor/CustomBuilderView.cs
+++ Task 1/Assets/Editor/CustomBuilderView.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 
 public class CustomBuilderView : EditorWindow {
 
-	string buildName = Application.productName;
+    static string buildName;
+    static string bundleIdentifier;
 
-	[MenuItem("JPT Application/Task 1/Custom Build Window")]
+    [MenuItem("JPT Application/Task 1/Custom Build Window")]
 	static void OpenBuildWindow(){
-		EditorWindow.GetWindow(typeof(CustomBuilderView));
+        SetUpGlobalVariables();
+
+        EditorWindow.GetWindow(typeof(CustomBuilderView));
 	}
+
+    static void SetUpGlobalVariables()
+    {
+        buildName = Application.productName;
+        bundleIdentifier = Application.identifier;
+    }
 
 	void OnGUI(){
-		
-		buildName = EditorGUILayout.TextField("Build Name:", buildName);
-	}
+        buildName = EditorGUILayout.TextField("Build Name:", buildName);
+        bundleIdentifier = EditorGUILayout.TextField("Bundle Identifier:", bundleIdentifier);
+        ValidateBundleIdentifier();
+    }
+
+    void ValidateBundleIdentifier()
+    {
+        string defaultBundleID = "com.Company.ProductName";
+        if (bundleIdentifier == defaultBundleID || 
+            bundleIdentifier == "")
+        {
+            // TODO add more checks (for example for special characters)
+            // Find out what characters bundle ID does not accept
+            EditorGUILayout.HelpBox("Build will fail with default or empty bundle identifier. Please, make sure it is not \"" + defaultBundleID + "\" or empty", MessageType.Warning);
+        }
+    }
 }
  
