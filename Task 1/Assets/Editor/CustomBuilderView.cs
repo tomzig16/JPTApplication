874f37939e1bf5cf63f23e9c98015aad55992e2a
tomzig16 <tzigmantavicius@gmail.com>
2018-03-28 18:19:50 +0300

    Init Unity project

---
+++ Task 1/Assets/Editor/CustomBuilderView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomBuilderView : EditorWindow {
+
+	string buildName = Application.productName;
+
+	[MenuItem("JPT Application/Task 1/Custom Build Window")]
+	static void OpenBuildWindow(){
+		EditorWindow.GetWindow(typeof(CustomBuilderView));
+	}
+
+	void OnGUI(){
+		
+		buildName = EditorGUILayout.TextField("Build Name:", buildName);
+	}
+}
+ 
